7324f44f4d5999a7292395fa04f83bbe675042f0
chashm-e-afreen <belta.infinita@yandex.com>
2025-12-09 18:41:46 +0000

    Add xdg desktop portal

--- Sandboxes/Waybar/sandbox_ironbar.rb
+++ Sandboxes/Waybar/sandbox_ironbar.rb
@@ -8,15 +8,16 @@ pwd = script_dir = __dir__
 
 args = [
   "bwrap",
-  "--ro-bind", "/usr/bin/dbus-run-session", "/usr/bin/dbus-run-session",
   "--ro-bind", "/usr/bin/dbus-daemon", "/usr/bin/dbus-daemon",
+  "--ro-bind", "/usr/bin/dbus-run-session", "/usr/bin/dbus-run-session",
   "--ro-bind", "/usr/bin/waybar", "/usr/bin/waybar",
+  "--ro-bind", "/usr/libexec/xdg-desktop-portal", "/usr/libexec//xdg-desktop-portal",
   "--ro-bind", "/usr/lib", "/usr/lib",
   "--symlink", "/usr/lib", "/lib",
   "--symlink", "/usr/lib", "/usr/lib64",
   "--ro-bind", "/usr/share/dbus-1", "/usr/share/dbus-1",
   "--ro-bind", "/usr/share/X11/xkb", "/usr/share/X11/xkb",
-  "--ro-bind", File.join(pwd, "fonts"), "/usr/share/fonts",
+  "--ro-bind", File.join(home, "Sandboxes", "Waybar", "fonts"), "/usr/share/fonts",
   "--ro-bind", "/bin/sh", "/bin/sh",
   "--ro-bind", niri, niri,
   "--ro-bind", "#{xdg}/#{wayland}", "#{xdg}/#{wayland}",
